3304c4429352abf47954fa593a851ed355817f7c
roberio <@gmail.comsenacsousaroberio>
2025-04-02 21:50:39 -0300

    meu setimo commit

---
+++ aula 1/Tarefa -  7.py
@@ -0,0 +1,9 @@
+def calcule_area_quadrada(lado):
+    return lado ** 2
+
+
+# Exemplo de uso
+lado =floant(impu)("Digite o valo do quadrado:"))
+area = calcular_area_quadrado(lado
+dobro_area = area* 2
+print(f"A área do quadrado é {area:.2f}")
