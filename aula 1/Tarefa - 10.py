0039e58fe5ca6a3abba389a478cf7478c7892b46
roberio <@gmail.comsenacsousaroberio>
2025-04-09 20:12:32 -0300

    estruturadedecisao commit

---
+++ aula 1/Tarefa - 10.py
@@ -0,0 +1,6 @@
+turno =
+
+print("-- Em que turno você estuda?")
+print("Digite m para matutino, v para vespetino e n para noturno")
+
+turno = input("Opção")
---
+++ estruturadedesisao/Tarefa - 6.py
@@ -0,0 +1,3 @@
+# Leitura dos três números
+num1 = float(input("Digite o primeiro número:"))
+num2 = float(input)
