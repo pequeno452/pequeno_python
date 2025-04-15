a264276ac186f0bb626ee6d37800a6ebf2bc2ab4
roberio <@gmail.comsenacsousaroberio>
2025-04-14 21:42:57 -0300

    estruturadedecisao commit

---
+++ estrutura-4/funcoes/Tarefa - 3.py
@@ -0,0 +1,10 @@
+def somar_tres_numeros(a, b, c):
+    return a + b + c
+
+# Exemplo de uso
+num1 =int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+num3 = int(input("Digite o terceiro número:"))
+
+resultado = somar_tres_numeros(num1, num2,num3)
+print("A soma dos três números é:", resultado)
